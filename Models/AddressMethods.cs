f4f6f270d220456f3011476dcac5e4894797809f
brendantyler <galmanbrendan@gmail.com>
2023-03-28 14:20:54 -0500

    Add Address Read

---
+++ Models/AddressMethods.cs
@@ -0,0 +1,22 @@
+﻿using adventure_works_project.Models;
+
+namespace adventure_works_project.Models
+{
+    public static class AddressMethods
+    {
+        public static IResult Read(int? id, AdventureWorksLt2019Context db)
+        {
+            if (id != null)
+            {
+                return Results.Ok(db.Addresses.FirstOrDefault(a => a.AddressId == id));
+            }
+
+            return Results.Ok(db.Addresses.ToList());
+        }
+    }
+}
+
+
+
+   
+
--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 using adventure_works_project.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AdventureWorksLt2019Context>();
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+//Address Read
+app.MapGet("/address/{id?}", AddressMethods.Read);
 
 app.Run();
