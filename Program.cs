d1c4359fa94f164755cd1a6cb0ff909effe8129e
Enjae Antonio <Enjaeantonio@outlook.com>
2023-03-28 17:40:27 -0500

    read and create endpoints initialized

--- Program.cs
+++ Program.cs
@@ -22,4 +22,37 @@ app.MapPost("/SalesOrderHeader/Create", async (SalesOrderHeader salesOrderHeader
     return Results.Created($"/SalesOrderHeader/{salesOrderHeader.SalesOrderId}", salesOrderHeader);
 });
 
+// Get all Products
+app.MapGet("/Product", (AdventureWorksLt2019Context context) =>
+{
+    List<Product> products = context.Products.ToList();
+    return Results.Ok(products);
+});
+
+// Get Product by primary key (ProductId)
+app.MapGet("/Product/productId", (int productId, AdventureWorksLt2019Context context) =>
+{
+    Product product = context.Products.Find(productId);
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(product);
+});
+
+// Get all SalesOrderHeaders
+app.MapGet("/SalesOrderHeader", (AdventureWorksLt2019Context context) =>
+{
+    List<SalesOrderHeader> salesOrderHeaders = context.SalesOrderHeaders.ToList();
+    return Results.Ok(salesOrderHeaders);
+});
+
+// Get SalesOrderHeader by primary key (SalesOrderId)
+app.MapGet("/SalesOrderHeader/salesOrderId", (int salesOrderId, AdventureWorksLt2019Context context) =>
+{
+    SalesOrderHeader salesOrderHeader = context.SalesOrderHeaders.Find(salesOrderId);
+    if (salesOrderHeader == null) return Results.NotFound();
+    return Results.Ok(salesOrderHeader);
+});
+
 app.Run();
