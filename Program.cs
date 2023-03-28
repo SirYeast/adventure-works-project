8c2ccc269a6fd2e5795094278feec3c77672acce
SirYeast <mkorzhan@outlook.com>
2023-03-28 12:50:55 -0500

    Add DbContext service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using adventure_works_project.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AdventureWorksLt2019Context>();
 var app = builder.Build();
 
 app.Run();
